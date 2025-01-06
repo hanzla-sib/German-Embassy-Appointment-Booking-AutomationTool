735d7a46f47bccf916970c042e1ea9e7ad4d1f4b
hanzla sibghat <hanzlasib24@gmail.com>
2025-01-06 01:26:19 +0100

    push

--- masterspickmultipleslots.py
+++ masterspickmultipleslots.py
@@ -48,7 +48,7 @@ def wait_until_4am():
     now = datetime.now()
     
     # Calculate time to 4:00:00 AM
-    target_time = now.replace(hour=0, minute=0, second=0, microsecond=0)
+    target_time = now.replace(hour=23, minute=59, second=54, microsecond=0)
 
     # If we've already passed 4 AM today, target tomorrow's 4 AM
     if now.time() >= target_time.time():
@@ -63,7 +63,7 @@ def wait_until_4am():
 
 
     # Open the target webpage
-driver.get("https://service2.diplo.de/rktermin/extern/appointment_showDay.do?locationCode=kara&realmId=967&categoryId=2801&dateStr=31.01.2025&openingPeriodId=68494")
+driver.get("https://service2.diplo.de/rktermin/extern/appointment_showDay.do?locationCode=kara&realmId=967&categoryId=2801&dateStr=31.01.2025")
 # Use WebDriverWait for dynamic content
 wait = WebDriverWait(driver, 10)
 
@@ -93,37 +93,61 @@ if base64_match:
         # Submit the form
         submit_button = driver.find_element(By.ID, 'appointment_captcha_day_appointment_showDay')
 
+        wait_until_4am()
         
         submit_button.click()
-        target_url = "https://service2.diplo.de/rktermin/extern/appointment_showForm.do?locationCode=kara&realmId=967&categoryId=2801&dateStr=31.01.2025&openingPeriodId=68494"
-        # wait_until_4am()
-        driver.get(target_url)
+        
+        #--------------------------------Correct till here--------------------------------
+        # Click the "Appointments are available" link
+        
+        while True:
+            try:
+                # Wait for the "Appointments are available" link
+                appointments_link = WebDriverWait(driver, 0.5).until(
+                    EC.presence_of_element_located((By.XPATH, "//a[contains(text(), 'this')]"))
+                )
+                
+                # If link is found, print success and click
+                print("Appointments are available!")
+                
+                appointments_link.click()
+                break
+            
+            except TimeoutException:
+                # If link not found, reload the page
+                print("No available appointments. Reloading...")
+                
+                driver.refresh()
+                
         
        
         
+        
+        element = WebDriverWait(driver,100).until(EC.presence_of_element_located((By.ID, "wwlbl_appointment_newAppointmentForm_lastname")))
+        
     
         lastname_input = driver.find_element(By.ID, 'appointment_newAppointmentForm_lastname')
-        last_name = "KHAN"  # Assuming this is from the previous 2Captcha result
+        last_name = "ABBAS"  # Assuming this is from the previous 2Captcha result
         lastname_input.send_keys(last_name)
 
 
         firstname_input = driver.find_element(By.ID, 'appointment_newAppointmentForm_firstname')
-        first_name = "AIMON"  # Assuming this is from the previous 2Captcha result
+        first_name = "HASSAN FARHAT"  # Assuming this is from the previous 2Captcha result
         firstname_input.send_keys(first_name)
 
 
         email_input = driver.find_element(By.ID, 'appointment_newAppointmentForm_email')
-        email = "aimonkhan24@gmail.com"  # Assuming this is from the previous 2Captcha result
+        email = "farhathassan489@gmail.com"  # Assuming this is from the previous 2Captcha result
         email_input.send_keys(email)
 
 
         email_input_repeat = driver.find_element(By.ID, 'appointment_newAppointmentForm_emailrepeat')
-        emailrepeat = "aimonkhan24@gmail.com"  # Assuming this is from the previous 2Captcha result
+        emailrepeat = "farhathassan489@gmail.com"  # Assuming this is from the previous 2Captcha result
         email_input_repeat.send_keys(emailrepeat)
 
         
         passportNumber_input = driver.find_element(By.ID, 'appointment_newAppointmentForm_fields_0__content')
-        passportnumber = "KN4145152"  # Assuming this is from the previous 2Captcha result
+        passportnumber = "JT1011253"  # Assuming this is from the previous 2Captcha result
         passportNumber_input.send_keys(passportnumber)
 
         Province_input = driver.find_element(By.ID, 'appointment_newAppointmentForm_fields_1__content')
@@ -150,9 +174,10 @@ if base64_match:
                 # Submit the form
                 submit_button = driver.find_element(By.ID, 'appointment_newAppointmentForm_appointment_addAppointment')
                 current_url = driver.current_url
-                print(f"Current URL BAchelors before submission: {current_url}")
-                # time.sleep(3)
-                # submit_button.click()
+                print(f"Current URL Masters before submission: {current_url}")
+                time.sleep(3)
+                submit_button.click()
+                
                 print("completed in = ", datetime.now()-datetime.now().replace(hour=0, minute=0, second=0, microsecond=0))
                 input("Press Enter to exit and close the browser...")
             except Exception as e:
