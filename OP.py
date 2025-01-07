03c42879161f6f081ebe888640d23a8ab810b2db
hanzla sibghat <hanzlasib24@gmail.com>
2025-01-07 23:19:40 +0100

    submit

--- OP.py
+++ OP.py
@@ -63,7 +63,7 @@ def wait_until_4am():
 
 
     # Open the target webpage
-driver.get("https://service2.diplo.de/rktermin/extern/appointment_showDay.do?locationCode=kara&realmId=1116&categoryId=2339&dateStr=04.02.2025")
+driver.get("https://service2.diplo.de/rktermin/extern/appointment_showDay.do?locationCode=kara&realmId=1116&categoryId=2339&dateStr=05.02.2025")
 # Use WebDriverWait for dynamic content
 wait = WebDriverWait(driver, 10)
 
