fed86a19ab34d74857d2e052394ba5b2aa112887
hanzla sibghat <hanzlasib24@gmail.com>
2025-01-08 23:51:00 +0100

    changes

--- BP.py
+++ BP.py
@@ -63,7 +63,7 @@ def wait_until_4am():
 
 
     # Open the target webpage
-driver.get("https://service2.diplo.de/rktermin/extern/appointment_showDay.do?locationCode=kara&realmId=967&categoryId=1988&dateStr=05.02.2025")
+driver.get("https://service2.diplo.de/rktermin/extern/appointment_showDay.do?locationCode=kara&realmId=967&categoryId=1988&dateStr=06.02.2025")
 # Use WebDriverWait for dynamic content
 wait = WebDriverWait(driver, 10)
 
